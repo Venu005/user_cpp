680ebb8413fdff60e6369456c7c5cf35c0160177
venusai <venusaiyalamanchili@gmail.com>
2024-11-23 23:33:03 +0530

    chore: initial setup

---
+++ controllers/user_app_User.cc
@@ -0,0 +1,11 @@
+#include "user_app_User.h"
+
+using namespace user_app;
+
+// Add definition of your processing function here
+
+void User::createUser(const HttpRequestPtr &req, std::function<void(const HttpResponsePtr &)> &&callback)
+{
+   
+   
+}
